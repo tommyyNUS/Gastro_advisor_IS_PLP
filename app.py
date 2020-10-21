45944e9d269ce43ed605cdca859912f9aff8b8b2
Tommy <tommyyyong88@hotmail.com>
2020-10-21 23:56:00 +0800

    Committing changes to app.py to handle non text and to process audio.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 from flask import Flask, request
+import requests
 import telegram
 from constants import bot_token, bot_user_name,URL
 
@@ -21,31 +22,29 @@ print("Initializing...")
 chatbot = initialize()
 
 #Check for Mode
-global MODE
-if (len(sys.argv) == 2): 
-    if (sys.argv[1]=="-debug"): MODE = "DEBUG"
-    elif (sys.argv[1]=="-train"): MODE = "TRAIN"
-else: MODE = "TELEGRAM"
+#global MODE
+#if (len(sys.argv) == 2): 
+#    if (sys.argv[1]=="-debug"): MODE = "DEBUG"
+#    elif (sys.argv[1]=="-train"): MODE = "TRAIN"
+#else: MODE = "TELEGRAM"
 
 #Set Mode
-if (MODE == "DEBUG"):
-    nlp = spacy.load("en_core_web_md", disable=["ner"])
-    print("Hello, you are in debug mode, how can i help you?")
-    while 1:
-        request = input("User: ")
+#if (MODE == "DEBUG"):
+#    nlp = spacy.load("en_core_web_md", disable=["ner"])
+#    print("Hello, you are in debug mode, how can i help you?")
+#    while 1:
+#        request = input("User: ")
 
-        intent_detected,intent,obj,location,graded_aspect = detect_intent(nlp,request)
-        if (intent_detected == 1): response = check_intent(intent,obj,location,graded_aspect)
-        else: response = chatbot.get_response(request)
+#        intent_detected,intent,obj,location,graded_aspect = detect_intent(nlp,request)
+#        if (intent_detected == 1): response = check_intent(intent,obj,location,graded_aspect)
+#        else: response = chatbot.get_response(request)
 
-        print("Bot: ", response)
-        if (intent=="ExitApp"): sys.exit()
+#        print("Bot: ", response)
+#        if (intent=="ExitApp"): sys.exit()
 
-elif (MODE == "TRAIN"): 
-    train_data(chatbot)
-    sys.exit()
-
-
+#elif (MODE == "TRAIN"): 
+#    train_data(chatbot)
+#    sys.exit()
 
 nlp = spacy.load("en_core_web_md", disable=["ner"])
 chatbot = initialize()
@@ -56,12 +55,24 @@ app = Flask(__name__)
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-
+    text = ""
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
-    text = update.message.text.encode('utf-8').decode()
-
+    print(update.message)
+    
+    if update.message.text != None:
+        text = update.message.text.encode('utf-8').decode()
+    if update.message.voice != None:
+        if update.message.voice.file_id != None:
+            file_id = update.message.voice.file_id
+            audio_file_object = bot.get_file(file_id)
+            file_path = audio_file_object['file_path']
+            audio_file = requests.get(file_path).content
+            #with open('output.wav', 'wb') as out:
+            #    out.write(audio_file)
+            #To process audio_file into text...
+    
     #Get response for restaurant recommendations
     intent_detected,intent,obj,location,graded_aspect = detect_intent(nlp,text)
 
