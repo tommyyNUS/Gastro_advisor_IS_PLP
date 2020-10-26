a4775604f30544c549d993cfc5b95716a7d5d8b6
Paul S.R <61770736+paulsr87@users.noreply.github.com>
2020-10-26 22:09:16 +0800

    Update DataBase.py

--- database/DataBase.py
+++ database/DataBase.py
@@ -37,7 +37,7 @@ class DataBase:
     def __init__(self, path=None):
         try:
             if path is None:
-                self.PATH = 'gastrotommy_v2.db'
+                self.PATH = 'gastrotommy_v3.db'
             else:
                 self.PATH = path
                 
